8c49ce0f8101d292e530abd67f503212f8d07d36
VenuSCode <58114938+VenuS-Code@users.noreply.github.com>
2023-05-02 15:15:56 +0530

    roboshop.conf update

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
 dnf module disable mysql -y
 cp /home/centos/roboshop1/mysql.repo /etc/yum.repos.d/mysql.repo
 yum install mysql-community-server -y
-mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
+mysql_secure_installation --set-root-pass Roboshop@1
+mysql -uroot -pRoboshop@1
